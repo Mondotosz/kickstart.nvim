2e42ef12a091422da4db70c431aa4432cfb05fe4
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-03-20 12:58:56 +0100

    updated cmake-tools config
    added keybinds for cmake-tools and configured lazyloading for the plugin requiring c or cmake files to be open

--- lua/custom/plugins/cmake-tools.lua
+++ lua/custom/plugins/cmake-tools.lua
@@ -1,20 +1,41 @@
 return {
-    'Civitasv/cmake-tools.nvim',
-    dependencies = { 'akinsho/toggleterm.nvim', 'stevearc/overseer.nvim' },
-    config = function()
-      local cmake = require 'cmake-tools'
-      cmake.setup {
-        cmake_executor = {
-          name = 'toggleterm',
-          opts = {},
-          default_opts = {
-            toggleterm = {
-              direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float'
-              close_on_exit = true, -- whether close the terminal when exit
-              auto_scroll = true, -- whether auto scroll to the bottom
-            },
+  'Civitasv/cmake-tools.nvim',
+  ft = { 'c', 'cmake' },
+  keys = {
+    {"<leader>cc", ":CMakeBuild<cr>", desc = "Cmake build"},
+    {"<leader>cr", ":CMakeRun<cr>", desc = "Cmake run"},
+    {"<leader>ct", ":CMakeSelectBuildTarget<cr>", desc = "Cmake select build target"},
+    {"<leader>cT", ":CMakeSelectBuildType<cr>", desc = "Cmake select build type"},
+  },
+  dependencies = { 'akinsho/toggleterm.nvim', 'stevearc/overseer.nvim' },
+  config = function()
+    local cmake = require 'cmake-tools'
+    cmake.setup {
+      cmake_build_directory = 'build/${variant:buildType}',
+      cmake_soft_link_compile_commands = false, -- this will automatically make a soft link from compile commands file to project root dir
+      cmake_compile_commands_from_lsp = true,
+      cmake_executor = {
+        name = 'toggleterm',
+        opts = {},
+        default_opts = {
+          toggleterm = {
+            direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float'
+            close_on_exit = true, -- whether close the terminal when exit
+            auto_scroll = true, -- whether auto scroll to the bottom
           },
         },
-      }
-    end,
-  }
+      },
+      cmake_runner = {
+        name = 'toggleterm',
+        opts = {}, -- the options the runner will get, possible values depend on the runner type. See `default_opts` for possible values.
+        default_opts = { -- a list of default and possible values for runners
+          toggleterm = {
+            direction = 'float', -- 'vertical' | 'horizontal' | 'tab' | 'float'
+            close_on_exit = false, -- whether close the terminal when exit
+            auto_scroll = true, -- whether auto scroll to the bottom
+          },
+        },
+      },
+    }
+  end,
+}
