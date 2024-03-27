72797dfc7ad32d6c11223512f03b5f4b2cfaf00e
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-03-27 19:15:21 +0100

    added custom terminal names

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -16,10 +16,23 @@ return {
       end
     end
 
-    -- numbered term
-    for i = 1, 9 do
-      vim.keymap.set('n', '<leader>t' .. i, ':ToggleTerm ' .. i .. ' direction=float name=' .. i .. '<cr>', { desc = 'ToggleTerm: Show terminal ' .. i })
+    local toggle_n = function (n, symbol)
+      return function ()
+        ensure_buffer_saved()
+        term.toggle_command('direction=float name=' .. symbol, n)
+      end
     end
+
+    -- numbered term
+    vim.keymap.set('n', '<leader>t1', toggle_n(1,'󰬺'), {desc = 'ToggleTerm: Show terminal 1'})
+    vim.keymap.set('n', '<leader>t2', toggle_n(2,'󰬻'), {desc = 'ToggleTerm: Show terminal 2'})
+    vim.keymap.set('n', '<leader>t3', toggle_n(3,'󰬼'), {desc = 'ToggleTerm: Show terminal 3'})
+    vim.keymap.set('n', '<leader>t4', toggle_n(4,'󰬽'), {desc = 'ToggleTerm: Show terminal 4'})
+    vim.keymap.set('n', '<leader>t5', toggle_n(5,'󰬾'), {desc = 'ToggleTerm: Show terminal 5'})
+    vim.keymap.set('n', '<leader>t6', toggle_n(6,'󰬿'), {desc = 'ToggleTerm: Show terminal 6'})
+    vim.keymap.set('n', '<leader>t7', toggle_n(7,'󰭀'), {desc = 'ToggleTerm: Show terminal 7'})
+    vim.keymap.set('n', '<leader>t8', toggle_n(8,'󰭁'), {desc = 'ToggleTerm: Show terminal 8'})
+    vim.keymap.set('n', '<leader>t9', toggle_n(9,'󰭂'), {desc = 'ToggleTerm: Show terminal 9'})
 
     vim.keymap.set({ 'v', 'i', 't', 'n' }, '¬', function()
       ensure_buffer_saved()
@@ -32,7 +45,7 @@ return {
 
     -- NOTE: LazyGit terminal
     if vim.fn.executable 'lazygit' then
-      local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float' }
+      local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float', display_name = 'LazyGit' }
 
       vim.keymap.set('n', '<leader>gg', function()
         ensure_buffer_saved()
