ed5c74ed9ccdeff36534f484baef3bf50ae597bd
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-03-30 12:22:44 +0100

    tweaked telescope project extension

--- lua/custom/plugins/telescope-project.lua
+++ lua/custom/plugins/telescope-project.lua
@@ -5,12 +5,12 @@ return {
     {
       '<leader>p',
       function()
-        require('telescope').extensions.project.project()
+        require('telescope').extensions.project.project { display_type = 'full' }
       end,
       desc = '[P]roject',
     },
   },
   config = function()
-    require'telescope'.load_extension 'project'
+    require('telescope').load_extension 'project'
   end,
 }
