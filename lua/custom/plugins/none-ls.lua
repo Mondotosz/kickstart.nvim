4e509b5484a1d54a5d43d9cea58bed16d9e6ac16
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-03-30 14:39:22 +0100

    changed severity for cspell to hint

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,15 +1,21 @@
 return {
   'nvimtools/none-ls.nvim',
   dependencies = {
-    'davidmh/cspell.nvim'
+    'davidmh/cspell.nvim',
   },
   config = function()
-    local cspell = require('cspell')
-    require('null-ls').setup{
+    local cspell = require 'cspell'
+    local null_ls = require 'null-ls'
+    null_ls.setup {
       sources = {
-        cspell.diagnostics,
+        cspell.diagnostics.with {
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.severity = vim.diagnostic.severity.HINT
+          end,
+        },
         cspell.code_actions,
       },
+      -- fallback_severity = vim.diagnostic.severity.HINT,
     }
-  end
+  end,
 }
