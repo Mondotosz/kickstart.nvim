12e93f5168e9c25c8269505f8a4bc30bbb22604e
Mondotosz <55015209+Mondotosz@users.noreply.github.com>
2024-03-30 14:11:35 +0100

    added keybinds to search notifications

--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,6 +1,9 @@
 return {
   'rcarriga/nvim-notify',
   event = 'VimEnter',
+  keys = {
+    { '<leader>sN', ':Telescope notify<cr>', desc = '[S]earch [N]otifications' },
+  },
   config = function()
     vim.notify = require 'notify'
   end,
